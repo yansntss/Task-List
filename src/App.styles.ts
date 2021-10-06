03a3f533cff2ec20fa4f0e5b59f908e3ff6e2a55
yansntss <yansantos.dev@gmail.com>
2021-10-06 15:30:35 -0300

    First-commit

---
+++ src/App.styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+//container geral
+export const Container = styled.div`
+    background-color: #17181f;
+    color: #797A81;
+    min-height: 100vh;
+`;
+
+//div que vai receber o conteudo
+export const Area = styled.div`
+    margin:auto;
+    max-width: 980px;
+    padding: 10px;
+`;
+
+//header
+export const Header = styled.h1`
+    margin: 0;
+    padding: 0;
+    color: #FFF;
+    text-align: center;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// C = Container
+import * as C from "./App.styles";
+import { useState } from "react";
+import { Item } from "./types/Item";
+import { ListItem } from "./components/ListItem";
+const App = () => {
+            //setList
+  const [list] = useState<Item[]>([
+    { id: 1, name: "comprar pão na padaria", done: false },
+    { id: 2, name: "Comprar suco de limão", done: false },
+  ]);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <C.Container>
+      <C.Area>
+        <C.Header> Task List </C.Header>
 
+        {/* área de adicionar nova tarefa */}
+
+        {/* Lista de tarefas */}
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
+      </C.Area>
+    </C.Container>
+  );
+};
 export default App;
